dba8b1c3dd6b107037a19e4b975c4af265e819be
pimou <pidujing@foxmail.com>
2026-06-14 01:26:52 +0800

    refactor:resume_vue

--- resume_vue/src/__tests__/App.spec.js
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { mount } from '@vue/test-utils';
-import App from '../App.vue';
-describe('App', () => {
-    it('mounts renders properly', () => {
-        const wrapper = mount(App);
-        expect(wrapper.find('.common-layout').exists()).toBe(true);
-    });
-});
